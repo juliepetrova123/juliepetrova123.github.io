90e2148e648038a5a830e2c38226d3fcf0b1b14b
juliepetrova123 <iuliia.d.petrova@gmail.com>
2024-11-15 00:49:20 +0300

    Add files via upload

--- components/menu/script.js
+++ components/menu/script.js
@@ -1,13 +1,4 @@
 function toggleMenu() {
     const menu = document.querySelector('.menu-hamburger');
     menu.classList.toggle('active');
-
-    // const bars = document.querySelectorAll('.bar');
-    // bars.forEach((bar, index) => {
-    //     if (menu.classList.contains('active')) {
-    //         bar.style.transform = `translateY(${index * 10}px) rotate(${index === 1 ? 180 : 0}deg)`;
-    //     } else {
-    //         bar.style.transform = 'translateY(0) rotate(0)';
-    //     }
-    // });
 }
